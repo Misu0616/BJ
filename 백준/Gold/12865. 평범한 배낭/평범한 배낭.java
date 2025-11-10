32d1e2c50c3b392f140df33305273ff25356f4a4
Misu0616 <163505992+Misu0616@users.noreply.github.com>
2025-11-10 16:42:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,12 +1,16 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, K, W, V, result; // N 물품 수, k 가방 무게 최대, w 물건 무게, v 물건 가치
+	static int N, K; // N 물품 수, k 가방 무게 최대, w 물건 무게, v 물건 가치
+	static int[] W;
+	static int[] V;
+	
 	static int[][] bag;
-	static int[][] memoi;
+	static int[] memoi;
 	static StringTokenizer st;
 	
 	public static void main(String[] args) throws Exception {
@@ -15,31 +19,25 @@ public class Main {
 		
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		bag = new int[N][2];
 		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+		W = new int[N+1];
+	    V = new int[N+1];
 
-			W = Integer.parseInt(st.nextToken());
-			V = Integer.parseInt(st.nextToken());
-			
-			bag[i][0] = W;
-			bag[i][1] = V;
-		} 
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
 		
-        memoi = new int[N+1][K+1];
-        
-		for (int i = 1; i <= N; i++) {
-            for (int w = 0; w <= K; w++) {
-            	memoi[i][w] = memoi[i-1][w]; // 선택하지 않음
-                
-                if (w - bag[i-1][0] >= 0) { // 선택 가능한 경우
-                	memoi[i][w] = Math.max(memoi[i][w], memoi[i-1][w - bag[i-1][0]] + bag[i-1][1]);
-                }
+        memoi = new int[K+1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int w = K; w >= W[i]; w--) {
+            	memoi[w] = Math.max(memoi[w], memoi[w - W[i]] + V[i]);
             }
         }
-        
-        System.out.println(memoi[N][K]);
+
+        System.out.println(memoi[K]);
 		
 	}
 
