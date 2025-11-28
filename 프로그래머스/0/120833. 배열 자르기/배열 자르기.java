16d0ac858b29794f0df1467aa0728dcbcf65638b
Misu0616 <163505992+Misu0616@users.noreply.github.com>
2025-11-28 17:43:30 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        
+        numbers = Arrays.copyOfRange(numbers, num1, num2+1);
+        
+        return numbers;
+    }
+}
