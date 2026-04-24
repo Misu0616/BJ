1e26ee5466cb83c9a67bc68042286a711d7cde5b
Misu0616 <163505992+Misu0616@users.noreply.github.com>
2026-04-24 09:16:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.62 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+  
+        Stack<Integer> s = new Stack<>();
+        
+        for(int i=0; i<arr.length; i++){
+            if(s.isEmpty() || s.peek() != arr[i]){
+                s.push(arr[i]);  
+            } 
+        }
+        
+        int[] answer = new int[s.size()];
+        
+        for(int i = 0; i < s.size(); i++) {
+            answer[i] = s.get(i);
+        }
+        
+        return answer;
+    }
+}
