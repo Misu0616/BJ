19f06b5af52d04afbf9307b53393f652856b18e5
Misu0616 <163505992+Misu0616@users.noreply.github.com>
2026-06-27 09:47:51 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS 
+WHERE NAME IN ('Lucy')
+OR  NAME IN ('Ella') 
+OR  NAME IN ('Pickle') 
+OR NAME IN ('Rogan') 
+OR NAME IN ('Sabrina') 
+OR NAME IN ('Mitty')
+ORDER BY ANIMAL_ID
